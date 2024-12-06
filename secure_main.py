eba132cdf4f2dab89dbecf6f00971cc17ed9c990
Daniel Salin <daniel.salin@gmail.com>
2024-12-06 13:49:45 +0100

    Fix validatin for pwd and username, add user logged in menu

--- secure_main.py
+++ secure_main.py
@@ -1,14 +1,37 @@
 from secure_operations import create_user_table, register_user, login_user
 
+# Make this into class instead
+user_info = {"username": None, "user_id": None}
+
 create_user_table()
 
-print("1. Register")
-print("2. Login")
-choice = input("Choose an option: ")
+menu_options = {"no_user":["1. Register", "2.Login"], "user_logged_in" : ["1. Logout", "2. Write Note"]}
 
-if choice == "1":
-    register_user()
-elif choice =="2":
-    login_user()
-else:
-    print("Invalid option!")
+while True:
+    if user_info["username"] is None:
+        for option in menu_options["no_user"]:     
+            print(option)
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            register_user()
+        elif choice =="2":
+            user_info= login_user()
+            break
+        else:
+            print("Invalid option!")
+    
+while True:
+    for option in menu_options["user_logged_in"]:     
+        print(option)
+    choice = input(f"Welcome {user_info["username"]}! Choose an option: ")
+
+    if choice == "1":
+        print("----IMPLEMENT LOGOUT FEATURE----")
+        break
+    elif choice =="2":
+        print("----IMPLEMENT WRITE NOTE FEATURE----")
+        break
+    else:
+        print("Invalid option!")
+        
--- secure_operations.py
+++ secure_operations.py
@@ -1,5 +1,6 @@
 import sqlite3
 import bcrypt
+from getpass import getpass
 
 # Create a table to store users with hashed passwords
 def create_user_table():
@@ -19,12 +20,21 @@ def create_user_table():
     
 def register_user():
     """Registers a new user with a hashed password"""
-    username = input("Enter your username: ").strip()
-    password = input("Enter your password: ").strip()
+    while True:
+        username = input("Enter your username: ").strip()
+        password = getpass("Enter your password: ").strip()
+        if validate_username(username) and validate_password(password):
+            break
+        else:
+            print("----------------------------")
+            print("Invalid username or password")
+            print("----------------------------")
+            print("username must be at least 3 characters in length and contain a digit and a letter")
+            print("password must be at least 8 characters in length and contain a digit and at least one instance each of an uppercase and lowercase letter")
+            print("----------------------------")
     
     #hantera lösenordet
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-    print(hashed_password)
     connection = sqlite3.connect("secure_users.db")
     cursor = connection.cursor()
     
@@ -41,19 +51,63 @@ def register_user():
 # SECURE: Verify user credentials with hashed password
 def login_user():
     """Verify user credentials and login."""
+
     username = input("Enter your username: ").strip()
-    password = input("Enter your password: ").strip()
+    password = getpass("Enter your password: ").strip()
     
     connection = sqlite3.connect("secure_users.db")
     cursor = connection.cursor()
     
     # Säkra SQL-frågor med parametriserade frågor
-    cursor.execute("SELECT password_hash FROM users WHERE username = ?;", (username,))
+    cursor.execute("SELECT password_hash, id FROM users WHERE username = ?;", (username,))
     result = cursor.fetchone()
-    
+            
     if result and bcrypt.checkpw(password.encode("utf-8"), result[0]):
+        user_info = {"username": username, "user_id": result[1]}
         print("Login successful!")
-        
+        return user_info
     else:
         print("Login failed!")
     connection.close()
+
+def validate_username(username: str) -> bool:
+    """Validate username based on length and contents (must contain letter and digit)"""
+    valid_length = False
+    has_letter = False
+    has_digit = False
+    if len(username) > 3:
+        valid_length = True
+    # Loop over username characters to check for letters and digits
+    for char in username:
+        if char.isalpha():
+            has_letter = True
+        if char.isdigit():
+            has_digit = True
+    # Collect conditions in list and use all to verify that they are all met   
+    # Returns True if all items are True or list is empty, otherwise returns false
+    return all([valid_length, has_letter, has_digit]) 
+
+def validate_password(password: str) -> bool:
+    """Validate username based on length and contents (must contain letter and digit)"""
+    valid_length = False
+    has_letter = False
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+    if len(password) > 8:
+        valid_length = True
+    # Loop over password characters to check for letters and digits
+    for char in password:
+        if char.isalpha():
+            has_letter = True
+        if char.isupper():
+            has_uppercase = True
+        if char.islower():
+            has_lowercase = True
+        if char.isdigit():
+            has_digit = True
+    # Collect conditions in list and use all to verify that they are all met   
+    # Returns True if all items are True or list is empty, otherwise returns false
+    return all([valid_length, has_letter, has_uppercase, has_lowercase, has_digit]) 
+
+def 
